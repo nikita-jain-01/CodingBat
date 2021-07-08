1decd0c97ec9bbdc471cf68d7d240938835b7872
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-08 16:01:32 +0530

    We can sleep in or not

--- Warmup-1/sleep_in.java
+++ Warmup-1/sleep_in.java
@@ -1,3 +1,19 @@
+/*
+The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are 
+on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in.
+
+Input :-
+        sleepIn(false, false)
+        sleepIn(true, false)
+        sleepIn(false, true)
+
+Output :- 
+        true
+        false
+        true
+*/
+
+
 public boolean sleepIn(boolean weekday, boolean vacation) {
   if(weekday==false || vacation==true)
     return true;
