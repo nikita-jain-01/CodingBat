c35b368612101cbd88d0cc642d6980b218a43848
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-09 13:09:30 +0530

    Absolute difference

---
+++ Warmup-1/diff_21.java
@@ -0,0 +1,22 @@
+/*
+Given an int n, return the absolute difference between n and 21, except return double the 
+absolute difference if n is over 21.
+
+Input :-
+        diff21(19)
+        diff21(10)
+        diff21(21)
+
+Output :-
+        2
+        11
+        0
+*/
+
+
+public int diff21(int n) {
+    if(n<=21)
+        return (21-n);
+    else
+        return 2*(n-21);
+}  
