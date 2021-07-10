bcb068f6b21bed58f07cb783929721022ebe6425
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-10 20:33:05 +0530

    Parrot is in trouble or not

---
+++ Warmup-1/parrot_trouble.java
@@ -0,0 +1,23 @@
+/*
+We have a loud talking parrot. The "hour" parameter is the current hour time in the range 0..23.
+We are in trouble if the parrot is talking and the hour is before 7 or after 20. Return true 
+if we are in trouble.
+
+Input :-
+        parrotTrouble(true, 6)
+        parrotTrouble(true, 7)
+        parrotTrouble(false, 6)
+
+Output :-
+        true
+        false
+        false
+*/
+
+
+public boolean parrotTrouble(boolean talking, int hour) {
+    if(talking==true && (hour<7 || hour>20))
+        return true;
+    else
+        return false;
+}  
