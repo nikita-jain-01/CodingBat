6d120c6e0b22d970acdd8269a2101be931f4b99d
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-15 01:48:31 +0530

    13 is an unlucky number

---
+++ Array-2/sum_13.java
@@ -0,0 +1,10 @@
+public int sum13(int[] nums) {
+    int sum=0,i;
+    for(i=0;i<nums.length;i++){
+      if(nums[i]==13)
+        i++;
+      else
+        sum=sum+nums[i];
+    }
+    return sum;
+}
