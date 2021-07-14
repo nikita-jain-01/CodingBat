0a3c07ffe5b83d37a6010cd105752a21c31e4114
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-14 14:04:23 +0530

    Difference of max and min element

---
+++ Array-2/big_diff.java
@@ -0,0 +1,15 @@
+public int bigDiff(int[] nums) {
+    int min,max,i;
+    min=nums[0];
+    max=nums[0];
+    for(i=0;i<nums.length;i++){
+      if(max<nums[i])
+      max=nums[i];
+      if(min>nums[i])
+      min=nums[i];
+    }
+    return (max-min);
+public class big_diff {
+    
+}
+}  
