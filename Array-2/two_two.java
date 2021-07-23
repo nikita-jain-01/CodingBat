6f2623a1a5cc889da5b40f8b5bcbd03f3859b7c5
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-07-24 00:38:00 +0530

    twoTwo

---
+++ Array-2/two_two.java
@@ -0,0 +1,24 @@
+public boolean twoTwo(int[] nums) {
+    int k=0,i;
+    if(nums.length>=2){
+      for(i=0;i<nums.length-1;i++){
+        if(nums[i]==2 && nums[i+1]==2)
+        {
+          k=0;
+          i++;
+        }
+        else if(nums[i]==2 && nums[i+1]!=2)
+        {
+          k=1;
+          return false;
+        }
+      }
+      if(nums[nums.length-1]==2 && nums[nums.length-2]!=2)
+        return false;
+    }
+    else if(nums.length>=1){
+      if(nums[0]==2)
+      return false;
+    }
+    return true;
+}  
