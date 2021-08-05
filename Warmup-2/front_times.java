5e55ab97af3f1b9949ac86253a8ef5e76c41410f
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-08-05 13:00:46 +0530

    frontTimes

---
+++ Warmup-2/front_times.java
@@ -0,0 +1,13 @@
+public String frontTimes(String str, int n) {
+    String res="";
+    if(str.length()>=3){
+      for(int i=1;i<=n;i++)
+        res=res+str.charAt(0)+str.charAt(1)+str.charAt(2)+"";
+      return res;
+    }
+    else{
+      for(int i=1;i<=n;i++)
+        res=res+str+"";
+      return res;
+    }
+}  
