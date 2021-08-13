66eced144509d6272e6b4390dd983f1a1c3894fc
nikita-jain-01 <72670446+njain876085@users.noreply.github.com>
2021-08-13 18:13:16 +0530

    inOrderEqual

---
+++ Logic-1/in_order_equal.java
@@ -0,0 +1,5 @@
+public public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+    if((a<b && b<c && a<c && equalOk==false) || (a<=b && b<=c && a<=c && equalOk==true))
+      return true;
+    return false;
+}
